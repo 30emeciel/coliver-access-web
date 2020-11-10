02637d7336a31b32294641b8d447ca5b7ee65125
Tony Lefebvre <tony.lbvre@gmail.com>
2020-11-10 14:46:22 +0100

    Refacto bis bis

--- src/app/PresenceCalendar/ColivingForm.tsx
+++ src/app/PresenceCalendar/ColivingForm.tsx
@@ -1,59 +1,41 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import "../Switch.css";
-import { DateTime, Duration, Interval } from "luxon";
-import { $enum } from "ts-enum-util";
-
-import {
-  Alert,
-  ButtonGroup,
-  Card,
-  Col,
-  Container,
-  Dropdown,
-  DropdownButton,
-  Form,
-  Modal,
-  ToggleButton,
-} from "react-bootstrap";
-import Switch from "react-switch";
-import db from "../../db";
 import admin from "firebase";
+import { DateTime, Duration, Interval } from "luxon";
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import db from "../../db";
 import firebase from "../../firebase_config";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-import Spinner from "react-bootstrap/Spinner";
-import TheCalendar from "./TheCalendar";
-
-type DocumentData = firebase.firestore.DocumentData;
+import "../Switch.css";
 
 const ColivingForm = ({
   arrivalDate,
   departureDate,
   disabledDays,
-  onSubmit
+  onSubmit,
 }: {
-  arrivalDate: Date|null;
-  departureDate: Date|null;
+  arrivalDate: Date | null;
+  departureDate: Date | null;
   disabledDays: Set<DateTime>;
   onSubmit: () => void;
 }) => {
-
   const currentUser = firebase.auth().currentUser!;
   console.assert(currentUser != null);
 
   const [isColivingFormSubmitting, setIsColivingFormSubmitting] = useState(
     false
   );
-  const [interval, setInterval] = useState<null|Interval>(null)
+  const [interval, setInterval] = useState<null | Interval>(null);
 
   useEffect(() => {
-      setInterval(!arrivalDate || !departureDate ? null : Interval.fromDateTimes(arrivalDate, departureDate))      
-  }, [arrivalDate, departureDate])
+    setInterval(
+      !arrivalDate || !departureDate
+        ? null
+        : Interval.fromDateTimes(arrivalDate, departureDate)
+    );
+  }, [arrivalDate, departureDate]);
 
   const submitColivingRequest = async () => {
     if (!arrivalDate || !departureDate) {
-        return
+      return;
     }
     setIsColivingFormSubmitting(true);
     const start = DateTime.fromJSDate(arrivalDate);
@@ -96,18 +78,21 @@ const ColivingForm = ({
 
     await Promise.all(promise_arr);
 
-    // When all done, reset the UI    
+    // When all done, reset the UI
     setIsColivingFormSubmitting(false);
     onSubmit();
   };
-  
+
   const numberOfNights = interval ? interval.count("days") - 1 : null;
   return (
     <>
-    <span>
-      {numberOfNights ? <>You are going to stay for {numberOfNights} nights</> : <>Pick your departure date</>}
-      </span>
-      {" "}
+      <span>
+        {numberOfNights ? (
+          <>You are going to stay for {numberOfNights} nights</>
+        ) : (
+          <>Pick your departure date</>
+        )}
+      </span>{" "}
       {isColivingFormSubmitting ? (
         <Button disabled variant="primary">
           Loading...
