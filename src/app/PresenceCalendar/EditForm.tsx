872f9c800a769bdee90b65e304bba9d324b8394f
Tony Lefebvre <tony.lbvre@gmail.com>
2020-12-08 15:35:21 +0100

    WiP

--- src/app/PresenceCalendar/EditForm.tsx
+++ src/app/PresenceCalendar/EditForm.tsx
@@ -1,19 +1,20 @@
 import admin from "firebase";
 import { DateTime, Duration, Interval } from "luxon";
 import React, { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import db from "../../db";
 import firebase from "../../firebase_config";
 import "../Switch.css";
 
 const EditForm = (
-  {
-    onClickDay,
-    onSubmit,
-  }:
-  { 
-    onClickDay: (d: Date) => void,
-    onSubmit: () => void,
+  daysLoading,
+  pendingDays,
+  disabledDays
+}: {
+  daysLoading: boolean;
+  pendingDays: Set<number>;
+  disabledDays: Set<DateTime>;
   }) => {
   const [isFormSubmitting, setIsFormSubmitting] = useState(false);
   const [newDays, setNewDays] = useState(new Set<Date>());
@@ -24,6 +25,20 @@ const EditForm = (
   
   return (
     <>
+    <Row>
+    <TheCalendar
+          daysLoading={daysLoading}
+          pendingDays={pendingDays}
+          isRangeMode={false}
+          calValue={calValue}
+          onChange={(d) => {
+            if (d instanceof Date) {
+              setCalValue(d)
+            }
+          }}          
+        />
+        </Row>
+        <Row>
       <span>Click on the dates you want to add or remove</span>{" "}
       {isFormSubmitting ? (
         <Button disabled variant="primary">
@@ -38,6 +53,7 @@ const EditForm = (
           Submit
         </Button>
       )}
+      </Row>
     </>
   );
   <p>Click on the days to add/remove days</p>;
