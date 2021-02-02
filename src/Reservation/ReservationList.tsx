8a600357a06dd84ee1adaee7d5f55b3787864bec
Tony Lefebvre <tony.lbvre@gmail.com>
2021-02-02 18:33:01 +0100

    feat: supervisor can see the name of the pax in the list reservation in pending review

--- src/Reservation/ReservationList.tsx
+++ src/Reservation/ReservationList.tsx
@@ -11,8 +11,8 @@ import {
   IconDefinition,
 } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Button, List, Popconfirm, Tag } from "antd"
-import { useContext, useState } from "react"
+import { Avatar, Button, Popconfirm, Space, Spin, Table, Tag } from "antd"
+import { useContext, useEffect, useState } from "react"
 import { useCollectionData } from "react-firebase-hooks/firestore"
 import db from "src/core/db"
 import myloglevel from "src/core/myloglevel"
@@ -26,9 +26,10 @@ import {
 import { useHistory } from "react-router-dom"
 import { TDay, TDayConverter, TDayState } from "../models/Day"
 import { DateTime } from "luxon"
+import { TPax, TPaxConverter } from "../models/Pax"
 
 const log = myloglevel.getLogger("ReservationList")
-
+const { Column, ColumnGroup } = Table
 
 export default function ReservationList({ isSupervisorMode = false }: { isSupervisorMode?: boolean }) {
   const pc = useContext(PaxContext)
@@ -49,7 +50,6 @@ export default function ReservationList({ isSupervisorMode = false }: { isSuperv
   )
 
 
-
   const state2fields: Record<TReservationRequestState, [string, string, IconDefinition]> = {
     "CONFIRMED": ["Confirmée", "green", faCheckCircle],
     "PENDING_REVIEW": ["En attente", "orange", faClock],
@@ -61,7 +61,7 @@ export default function ReservationList({ isSupervisorMode = false }: { isSuperv
   }
 
 
-  const ListItem = ({item, children}: {item:TReservationRequest, children: any}) => {
+  const ActionButtons = ({ item }: { item: TReservationRequest }) => {
     const [isConfirmationSubmitting, setIsConfirmationSubmitting] = useState(false)
 
     const confirmReservation = async () => {
@@ -119,70 +119,102 @@ export default function ReservationList({ isSupervisorMode = false }: { isSuperv
     }
 
 
-    const actions = (item: TReservationRequest) => {
-      const actions = [
-        <Button size="small"
-                icon={<FontAwesomeIcon icon={faEdit} />}
-                onClick={() => history.push(`/reservation/${item.id}`)}>Modifier</Button>,
-        <Popconfirm
-          arrowPointAtCenter
-          onConfirm={cancelConfirmation}
-          title="Est-ce que tu veux annuler cette réservation ?"
-          okText="Oui"
-          cancelText="Non">
-          <Button
-            danger
-            size="small"
-            loading={isCancelingSubmitting}
-            icon={<FontAwesomeIcon icon={faExclamationCircle} />}>Annuler</Button>
-        </Popconfirm>,
-      ]
-      if (isSupervisorMode) {
-        const confirm = <Popconfirm
-          placement="topLeft"
-          onConfirm={confirmReservation}
-          title="Est-ce que tu veux confirmer cette réservation ?"
-          okText="Oui"
-          cancelText="Non"
-        ><Button size="small"
-                 loading={isConfirmationSubmitting}
-                 type="primary"
-                 icon={<FontAwesomeIcon icon={faCheckDouble} />}>Confirmer</Button>
-        </Popconfirm>
-        actions.push(confirm)
-      }
-      return actions
+    const actions = [
+      <Button size="small"
+              icon={<FontAwesomeIcon icon={faEdit} />}
+              onClick={() => history.push(`/reservation/${item.id}`)}>Modifier</Button>,
+      <Popconfirm
+        arrowPointAtCenter
+        onConfirm={cancelConfirmation}
+        title="Est-ce que tu veux annuler cette réservation ?"
+        okText="Oui"
+        cancelText="Non">
+        <Button
+          danger
+          size="small"
+          loading={isCancelingSubmitting}
+          icon={<FontAwesomeIcon icon={faExclamationCircle} />}>Annuler</Button>
+      </Popconfirm>,
+    ]
+    if (isSupervisorMode) {
+      const confirm = <Popconfirm
+        placement="topLeft"
+        onConfirm={confirmReservation}
+        title="Est-ce que tu veux confirmer cette réservation ?"
+        okText="Oui"
+        cancelText="Non"
+      ><Button size="small"
+               loading={isConfirmationSubmitting}
+               type="primary"
+               icon={<FontAwesomeIcon icon={faCheckDouble} />}>Confirmer</Button>
+      </Popconfirm>
+      actions.push(confirm)
     }
+    return <Space>{actions}</Space>
 
-    return <List.Item actions={actions(item)} extra={[]}>{children}</List.Item>
+  }
+
+  const [paxList] = useCollectionData<TPax>(isSupervisorMode ? db.collection("/pax").withConverter(TPaxConverter) : null)
+
+  const [paxMap, setPaxMap] = useState(new Map<string, TPax>())
+  useEffect(() => {
+    if (!paxList)
+      return
+    const m = paxList.reduce((previousValue, currentValue, index, arr) => {
+      previousValue.set(currentValue.sub, currentValue)
+      return previousValue
+    }, new Map<string, TPax>())
+    setPaxMap(m)
+  }, [paxList, setPaxMap])
+
+  const PaxField = ({ pax }: { pax: TPax | undefined }) => {
+
+    if (!pax)
+      return <Spin />
+
+    return <Space direction="horizontal">
+      <Avatar src={pax.picture} />
+      <span><strong>{pax.name}</strong></span>
+    </Space>
+  }
+
+  const reservationText = (item: TReservationRequest) => {
+    if (item.kind == TReservationRequestKind.COWORKING) {
+      return `le ${item.arrivalDate.setLocale("fr-fr").toLocaleString(DateTime.DATE_MED)}`
+    }
+    else {
+      return `du ${item.arrivalDate.setLocale("fr-fr").toLocaleString(DateTime.DATE_MED)} au ${item.departureDate!.setLocale("fr-fr").toLocaleString(DateTime.DATE_MED)} (${item.numberOfNights!} nuits)`
+    }
   }
 
   return (
     <>
       <h2>
         {isSupervisorMode ?
-          <><FontAwesomeIcon icon={faCheckDouble} /> Réservations en attente</>
+          <><FontAwesomeIcon icon={faCheckDouble} /> Réservations en attente de confirmation</>
           :
           <><FontAwesomeIcon icon={faBookReader} /> Mes réservations</>
         }
       </h2>
-      <List
-        itemLayout="horizontal"
-        bordered={true}
-        dataSource={listRequests}
-        loading={listRequestsLoading}
-        renderItem={(item) => {
-          const stateFields = state2fields[item.state] || ["?", "pink", faQuestionCircle]
-          const kindFields = kind2fields[item.kind] || ["?", "pink", faQuestionCircle]
-          return <ListItem item={item}>
-            <Tag color={kindFields[1]}><FontAwesomeIcon icon={kindFields[2]} /> {kindFields[0]}</Tag>
-            {item.arrivalDate.setLocale('fr-fr').toLocaleString(DateTime.DATE_MED)}
-            {item.departureDate && <> au {item.departureDate.setLocale('fr-fr').toLocaleString(DateTime.DATE_MED)} ({item.numberOfNights} nuits)</>}
-            {" "}
-            <Tag color={stateFields[1]}><FontAwesomeIcon icon={stateFields[2]} /> {stateFields[0]}</Tag>
-          </ListItem>
-        }}
-      />
+      <Table bordered={true} dataSource={listRequests} loading={listRequestsLoading} pagination={false}>
+        <Column title="Type" key="kind" dataIndex="kind" render={(kind: TReservationRequestKind) => {
+          const kindFields = kind2fields[kind] || ["?", "pink", faQuestionCircle]
+          return <Tag color={kindFields[1]}><FontAwesomeIcon icon={kindFields[2]} /> {kindFields[0]}</Tag>
+        }} />
+        {isSupervisorMode &&
+        <Column title="Pax" key="paxId" dataIndex="paxId" render={paxId => <PaxField pax={paxMap?.get(paxId)} />} />}
+        <Column key="text"
+                title="Description"
+                render={(text, record:TReservationRequest) => reservationText(record)} />
+        <Column title="Status" key="state" dataIndex="state" render={(state: TReservationRequestState) => {
+          const stateFields = state2fields[state] || ["?", "pink", faQuestionCircle]
+          return <Tag color={stateFields[1]}><FontAwesomeIcon icon={stateFields[2]} /> {stateFields[0]}</Tag>
+        }} />
+
+
+
+        <Column key="actions" title="Actions" render={(text, record:TReservationRequest) => <ActionButtons item={record} />} />
+      </Table>
     </>
   )
 }
